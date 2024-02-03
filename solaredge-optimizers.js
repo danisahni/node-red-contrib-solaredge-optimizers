9c365204c60483c49f15732d2175ee1181171569
danisahni <danielbuchberger@hotmail.de>
2024-02-03 22:31:09 +0100

    add functionality to scrape additional information from solaredge cloud add functionality to format the node's output to influxdb bulk data format

--- solaredge-optimizers.js
+++ solaredge-optimizers.js
@@ -4,16 +4,36 @@ const CookieJar = require('tough-cookie').CookieJar;
 
 const LOGIN_URL = 'https://monitoring.solaredge.com/solaredge-apigw/api/login';
 const DATA_URL = 'https://monitoring.solaredge.com/solaredge-web/p/playbackData';
+const DETAILS_URL = 'https://monitoring.solaredge.com/solaredge-web/p/systemData';
 
 module.exports = function (RED) {
     function SolaredgeOptimizersNode(config) {
         RED.nodes.createNode(this, config);
         this.siteId = config.siteId;
         this.timeUnit = config.timeUnit;
+        this.collectAdditionalInfo = config.collectAdditionalInfo;
+        this.formatForInfluxDb = config.formatForInfluxDb;
+        this.influxDbMeasurement = config.influxDbMeasurement;
         var node = this;
         node.on('input', async function (msg, send, done) {
             try {
-                let data = await getData(this.credentials.username, this.credentials.password, this.siteId, this.timeUnit);
+                const client = createClientWithCookieJar();
+                const token = await login(client, this.credentials.username, this.credentials.password);
+                let data = await getData(client, token, this.siteId, this.timeUnit);
+                if (this.collectAdditionalInfo) {
+                    let reporterIds = data.map(x => x.reporterId);
+                    let tags = await getTags(client, token, this.siteId, reporterIds)
+                    data = data.map(x => {
+                        let currentTags = tags.find(y => y.reporterId == x.reporterId);
+                        if (currentTags) {
+                            Object.assign(x, currentTags);
+                        }
+                        return x;
+                    })
+                }
+                if (this.formatForInfluxDb) {
+                    data = convertToInflux(data, this.influxDbMeasurement)
+                }
                 msg.payload = data;
             } catch (error) {
                 node.warn('error while fetching optimizer data')
@@ -25,9 +45,13 @@ module.exports = function (RED) {
         return;
     }
 
-    async function getData(username, password, siteId, timeUnit) {
+    function createClientWithCookieJar() {
         const jar = new CookieJar();
         const client = wrapper(axios.create({ jar }));
+        return client;
+    }
+
+    async function login(client, username, password) {
         let x_csrf_token = undefined;
         try {
             const params = new URLSearchParams();
@@ -39,10 +63,13 @@ module.exports = function (RED) {
                 }
             });
             x_csrf_token = response.headers['x-csrf-token'];
-            console.log(response);
         } catch (error) {
             console.log(error)
         }
+        return x_csrf_token;
+    }
+
+    async function getData(client, x_csrf_token, siteId, timeUnit) {
         try {
             const params = new URLSearchParams();
             params.append("fieldId", siteId);
@@ -53,12 +80,46 @@ module.exports = function (RED) {
                     'X-CSRF-TOKEN': x_csrf_token,
                 }
             })
+
             let data = cleanUpData(response.data);
             return data;
         }
         catch (error) {
             console.log(error);
         }
+    }
+
+    async function getTags(client, x_csrf_token, siteId, reporterIds) {
+        let tags = []
+
+        await Promise.all(reporterIds.map(async id => {
+            const params = new URLSearchParams();
+            params.append('fieldId', siteId);
+            params.append('reporterId', id);
+            params.append('type', 'any');
+            params.append('activeTab', '0');
+            params.append('isPublic', false);
+            try {
+                response = await client.post(DETAILS_URL, params, {
+                    headers: {
+                        'X-CSRF-TOKEN': x_csrf_token,
+                    }
+                });
+                let text = response.data.split('=')[1].split(';')[0];
+                let infoData = JSON.parse(text);
+                infoData.reporterId = id;
+                let tags_to_extract = ['reporterId', 'serialNumber', 'type', 'description', 'manufacturer'];
+                let currentTags = tags_to_extract.reduce((prev, curr) => {
+                    prev[curr] = infoData[curr];
+                    return prev;
+                }
+                    , {});
+                tags.push(currentTags);
+            } catch (error) {
+                console.log(error);
+            }
+        }))
+        return tags;
     }
 
     function cleanUpData(text) {
@@ -68,18 +129,43 @@ module.exports = function (RED) {
         text = text.replaceAll('timeUnit', '"timeUnit"').replaceAll('fieldData', '"fieldData"').replaceAll('reportersData',
             '"reportersData"');
         jsonData = JSON.parse(text);
-        data = {}
+        data = []
         for (const dateString of Object.keys(jsonData['reportersData'])) {
             let date = new Date(dateString);
             for (const sid of Object.keys(jsonData['reportersData'][dateString])) {
                 for (const entry of Object.values(jsonData['reportersData'][dateString][sid])) {
-                    if (!(entry['key'] in data)) { data[entry['key']] = {} }
+                    let index = data.findIndex(x => x.reporterId == entry['key']);
+                    if (index == -1) {
+                        data.push({ reporterId: entry['key'], data: [] })
+                        index = data.length - 1;
+                    }
+                    // if (!(entry['key'] in data)) { data[entry['key']] = {} }
                     let value = Number.parseFloat(entry['value'].replaceAll(',', ''));
-                    data[entry['key']][date.toISOString()] = value;
+                    data[index].data.push({ timestamp: date.toISOString(), value })
+                    // data[entry['key']][date.toISOString()] = value;
                 }
             }
         }
         return data;
+    }
+
+    function convertToInflux(data, measurement) {
+        influxData = [];
+        data.forEach(item => {
+            let tags = (({ data, ...o }) => o)(item)
+            item.data.forEach(entry => {
+                let influxEntry = {
+                    measurement,
+                    fields: {
+                        power: entry.value,
+                    },
+                    tags: tags,
+                    timestamp: Date.parse(entry.timestamp), // unix timestamp
+                };
+                influxData.push(influxEntry);
+            })
+        });
+        return influxData;
     }
 
     RED.nodes.registerType("solaredge-optimizers", SolaredgeOptimizersNode, {
